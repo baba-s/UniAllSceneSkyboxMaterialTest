9a3b9a063c7862b26d4bc224bfa2dd1137221b33
baba_s <shotababa1206@gmail.com>
2020-09-24 22:04:40 +0900

    first commit

---
+++ Editor/AllSceneSkyboxMaterialTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	internal sealed class AllSceneSkyboxMaterialTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void Skybox_Material_が無効になっているか()
+		{
+			AllSceneTester.Test( _ => RenderSettings.skybox == null );
+		}
+	}
+}
